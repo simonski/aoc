3944dd2d99ec600b82c065555eb9d970e5c0da54
Simon Gauld <simongauld@covey.home>
2020-12-06 09:00:23 +0000

    day 6 part 2 implemented and committed.

--- aoc2020_06.go
+++ aoc2020_06.go
@@ -11,12 +11,19 @@ import (
 // AOC_2020_06 is the entrypoint to the various attempts for day six
 func AOC_2020_06(cli *goutils.CLI) {
 	AOC_2020_06_part1_attempt1(cli)
+	AOC_2020_06_part2_attempt1(cli)
 }
 
 func AOC_2020_06_part1_attempt1(cli *goutils.CLI) {
 	filename := cli.GetFileExistsOrDie("-input")
 	q := NewQandAFromFile(filename)
 	fmt.Printf("There are %v total answers.\n", q.TotalForAllGroups())
+}
+
+func AOC_2020_06_part2_attempt1(cli *goutils.CLI) {
+	filename := cli.GetFileExistsOrDie("-input")
+	q := NewQandAFromFile(filename)
+	fmt.Printf("There are %v total answers in the second round.\n", q.SecondTotalForAllGroups())
 }
 
 type QandA struct {
@@ -46,6 +53,51 @@ func (q *QandA) TotalForAllGroups() int {
 		}
 	}
 	grandTotal += len(answers)
+	return grandTotal
+}
+
+func CheckGroupAnwsers(groupSize int, answers map[string]int) int {
+	total := 0
+	for key := range answers {
+		value := answers[key]
+		if value == groupSize {
+			total++
+		}
+	}
+	return total
+}
+
+func (q *QandA) SecondTotalForAllGroups() int {
+	// for each group, it is the number of questions which yes count matches the group size
+	answers := make(map[string]int)
+	grandTotal := 0
+	groupSize := 0
+	for index := 0; index < len(q.lines); index++ {
+		line := q.lines[index]
+		line = strings.TrimSpace(line)
+		if line == "" {
+			// new line, take the last answers and get the totals from them
+			grandTotal += CheckGroupAnwsers(groupSize, answers)
+			answers = make(map[string]int)
+			groupSize = 0
+		} else {
+			groupSize++
+			for position := 0; position < len(line); position++ {
+				key := line[position : position+1]
+				if q.ValidCharacter(key) {
+					count, exists := answers[key]
+					if exists {
+						count++
+					} else {
+						count = 1
+					}
+					answers[key] = count
+				}
+			}
+		}
+
+	}
+	grandTotal += CheckGroupAnwsers(groupSize, answers)
 	return grandTotal
 }
 
--- aoc2020_06_test.go
+++ aoc2020_06_test.go
@@ -31,6 +31,51 @@ func Test_AOC2020_06_BoardingPass(t *testing.T) {
 
 }
 
+func Test_AOC2020_06_BoardingPass2(t *testing.T) {
+
+	verifyAnswerCounts2("abc", 3, t)
+	verifyAnswerCounts2("b", 1, t)
+	verifyAnswerCounts2("za", 2, t)
+	verifyAnswerCounts2("atf", 3, t)
+	verifyAnswerCounts2("a", 1, t)
+	verifyAnswerCounts2("aA", 1, t)
+	verifyAnswerCounts2("a123", 1, t)
+	verifyAnswerCounts2(":a_", 1, t)
+	verifyAnswerCounts2(" a ", 1, t)
+	verifyAnswerCounts2("	a	", 1, t)
+	verifyAnswerCounts2("", 0, t)
+	verifyAnswerCounts2("	", 0, t)
+	verifyAnswerCounts2("  	", 0, t)
+	verifyAnswerCounts2("  	   ", 0, t)
+	verifyAnswerCounts2("  	   \\", 0, t)
+	verifyAnswerCounts2("  	   !", 0, t)
+
+	content := `abcx
+	abcy
+	abcz`
+
+	verifyAnswerCounts2(content, 3, t)
+
+	content = `abc
+
+	a
+	b
+	c
+	
+	ab
+	ac
+	
+	a
+	a
+	a
+	a
+	
+	b`
+
+	verifyAnswerCounts2(content, 6, t)
+
+}
+
 // verifyQNA helper to check multiple test cases
 func verifyAnswerCounts(line string, expected int, t *testing.T) {
 	qna := NewQandA(line)
@@ -38,5 +83,15 @@ func verifyAnswerCounts(line string, expected int, t *testing.T) {
 	if actual != expected {
 		// t.Errorf("Expected %v, got %v\n\n%v\n\n%v\n\n", expected, actual, line, qna.answers)
 		t.Errorf("Expected %v, got %v\n\n%v\n\n", expected, actual, line)
+	}
+}
+
+// verifyQNA helper to check multiple test cases
+func verifyAnswerCounts2(line string, expected int, t *testing.T) {
+	qna := NewQandA(line)
+	actual := qna.SecondTotalForAllGroups()
+	if actual != expected {
+		// t.Errorf("Expected %v, got %v\n\n%v\n\n%v\n\n", expected, actual, line, qna.answers)
+		t.Errorf("SecondTotalForAllGroups Expected %v, got %v\n\n%v\n\n", expected, actual, line)
 	}
 }
