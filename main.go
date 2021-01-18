fa58deec09951e63404415c36fc5f880cb73feee
simon <simonski@gmail.com>
2021-01-18 22:18:15 +0000

    graphics n stuff poc

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strconv"
 
 	goutils "github.com/simonski/goutils"
 )
@@ -15,11 +16,11 @@ Usage:
 	
 The commands are:
 
-    run (year) (day)         run a puzzle 
+    run    (year) (day)      run a puzzle 
 	
     render (year) (day)      render a puzzle to an animated gif
 
-    list (year)              list all the puzzles done so far
+    list   (year)            list all the puzzles done so far
 	
     version                  prints aoc version
 	
@@ -27,25 +28,205 @@ Usage "aoc help <topic>" for more information.
 
 `
 
-type AOC2015_06 struct {
-}
-
-func (logic *AOC2015_06) Part1(app *Application) {
-}
-
 type Application struct {
+	CLI   goutils.CLI
+	value string
+}
+
+func (app *Application) Run(cli *goutils.CLI) {
+	USAGE := "Usage: aoc run (year) (day)"
+	// fmt.Printf("Application.Run(%v)", cli.Args)
+	year := cli.GetStringOrDefault("run", "")
+	if year == "" {
+		fmt.Printf("%v\n", USAGE)
+		os.Exit(1)
+	}
+
+	day := cli.GetStringOrDefault(year, "01")
+
+	iyear, _ := strconv.Atoi(year)
+	iday, _ := strconv.Atoi(day)
+
+	methodNamePart1 := fmt.Sprintf("Y%vD%02dP1", iyear, iday)
+	methodNamePart2 := fmt.Sprintf("Y%vD%02dP2", iyear, iday)
+
+	_, m1, m1exists := app.GetMethod(methodNamePart1)
+	_, m2, m2exists := app.GetMethod(methodNamePart2)
+
+	if m1exists {
+		// fmt.Printf("%v, %v %v Part 1 exists, calling.\n", methodNamePart1, year, day)
+		m1.Call([]reflect.Value{})
+	} else {
+		fmt.Printf("%v(), %v %v Part 1 does not exist.\n", methodNamePart1, year, day)
+	}
+
+	if m2exists {
+		// fmt.Printf("%v, %v %v Part 2 exists, calling.\n", methodNamePart2, year, day)
+		m2.Call([]reflect.Value{})
+	} else {
+		fmt.Printf("%v(), %v %v Part 2 does not exist.\n", methodNamePart2, year, day)
+	}
+
+}
+
+func (app *Application) Y2015D01P1() {
+	fmt.Printf("Y2015D01P1() called\n")
+}
+
+func (app Application) GetMethod(methodName string) (reflect.Value, reflect.Value, bool) {
+
+	// var a Application
+	rvalue := reflect.ValueOf(&app)
+	// fmt.Printf("GetMethod[%v], rvalue %v\n", methodName, rvalue)
+	mvalue := rvalue.MethodByName(methodName)
+	// fmt.Printf("GetMethod[%v], mvalue %v\n", methodName, mvalue)
+	exists := false
+	if reflect.Value.IsValid(mvalue) {
+		exists = true
+	}
+
+	// if mvalue == nil {
+	// exists = false
+	// }
+	// cvalue := mvalue.Call([]reflect.Value{})
+	// fmt.Printf("cvalue %v\n", cvalue)
+	// if false {
+	// 	fmt.Printf("rvalue: %v, mvalue %v, cvalue %v\n", rvalue, mvalue, cvalue)
+	// }
+	return rvalue, mvalue, exists
+
+}
+
+func repeatstring(s string, times int) string {
+	out := s
+	for index := 0; index < times; index++ {
+		out += s
+	}
+	return out
+}
+
+func (app *Application) List(cli *goutils.CLI) {
+	// USAGE := "Usage: aoc list (year) (day)"
+	// fmt.Printf("Application.List(%v)", cli.Args)
+	// year := cli.GetStringOrDefault("run", "")
+	// if year == "" {
+	// 	fmt.Printf("%v\n", USAGE)
+	// 	os.Exit(1)
+	// }
+
+	max_year := 2021
+
+	dash := "\u2501"
+
+	header := fmt.Sprintf("\u2503      \u2503")
+	for day := 1; day <= 25; day++ {
+		header = fmt.Sprintf("%v%02d\u2503", header, day)
+	}
+
+	//https://en.wikipedia.org/wiki/Box-drawing_character
+	biggydown := "\u2533" + dash + dash
+	biggyup := "\u2543" + dash + dash
+	biggyuponly := "\u253b" + dash + dash
+
+	// repeat := 80
+	// bigline1 := "\u250f" + repeatstring(dash, repeat) + "\u2513"
+	bigline1 := "\u250F" + repeatstring(dash, 5) + repeatstring(biggydown, 24) + "\u2513"
+	// bigline2 := "\u2523" + repeatstring(dash, repeat) + "\u252b"
+	bigline2 := "\u2523" + repeatstring(dash, 5) + repeatstring(biggyup, 24) + "\u252b"
+	bigline3 := "\u2517" + repeatstring(dash, 5) + repeatstring(biggyuponly, 24) + "\u251b"
+	fmt.Println(bigline1)
+	fmt.Println(header)
+	fmt.Println(bigline2)
+	for year := 2015; year <= max_year; year++ {
+		line := fmt.Sprintf("\u2503 %04d \u2503", year)
+		for day := 1; day <= 25; day++ {
+			methodNamePart1 := fmt.Sprintf("Y%vD%02dP1", year, day)
+			methodNamePart2 := fmt.Sprintf("Y%vD%02dP2", year, day)
+			// methodNamePart1Render := fmt.Sprintf("Y%vD%02dP1Render", year, day)
+			// methodNamePart2Render := fmt.Sprintf("Y%vD%02dP2Render", year, day)
+
+			_, _, m1exists := app.GetMethod(methodNamePart1)
+			_, _, m2exists := app.GetMethod(methodNamePart2)
+			// _, _, m1existsRender := app.GetMethod(methodNamePart1Render)
+			// _, _, m2existsRender := app.GetMethod(methodNamePart2Render)
+
+			tick := "\u2713"
+			cross := "\u2717"
+
+			part1 := cross
+			part2 := cross
+			if m1exists {
+				part1 = tick
+				// if m1existsRender {
+				// 	part1 = tick
+				// }
+				// } else {
+				// 	part1 = tick + tick
+			} else {
+				part1 = cross
+			}
+
+			if m2exists {
+				part2 = tick
+			} else {
+				part2 = cross
+			}
+			// 	if m2existsRender {
+			// 		part2 = tick
+			// 	}
+			// } else {
+			// 	part2 = tick + tick
+
+			// }
+
+			line = fmt.Sprintf("%v%v%v\u2503", line, part1, part2)
+
+		}
+		fmt.Println(line)
+	}
+	fmt.Println(bigline3)
+
+}
+
+func (app *Application) Render(cli *goutils.CLI) {
+	USAGE := "Usage: aoc render (year) (day)"
+	// fmt.Printf("Application.Render(%v)", cli.Args)
+	year := cli.GetStringOrDefault("run", "")
+	if year == "" {
+		fmt.Printf("%v\n", USAGE)
+		os.Exit(1)
+	}
+}
+
+func (app *Application) Help(cli *goutils.CLI) {
+	fmt.Printf("Application.Help(%v)", cli.Args)
 }
 
 func (app *Application) Foo() {
-	fmt.Printf("Application.Foo()")
+	fmt.Printf("Application[value=%v].Foo()\n", app.value)
 }
 
-func reflectycall() {
+func NewApplication(cli goutils.CLI) Application {
+	app := Application{CLI: cli}
+	app.value = "hi there"
+	return app
+}
+
+// func Test(cli *goutils.CLI) {
+// 	app := NewApplication(cli)
+// 	reflectycall(app)
+// }
+
+func reflectycall(app Application) {
 	// gimme an application
-	var app Application
+	// var app Application
+	// var a Application
 	rvalue := reflect.ValueOf(&app)
-	mvalue := rvalue.MethodByName("foo")
+	fmt.Printf("rvalue %v\n", rvalue)
+	mvalue := rvalue.MethodByName("Foo")
+	fmt.Printf("mvalue %v\n", mvalue)
 	cvalue := mvalue.Call([]reflect.Value{})
+	fmt.Printf("cvalue %v\n", cvalue)
 	if false {
 		fmt.Printf("rvalue: %v, mvalue %v, cvalue %v\n", rvalue, mvalue, cvalue)
 	}
@@ -81,11 +262,35 @@ func main() {
 		Help(&cli)
 		os.Exit(1)
 	}
+	app := NewApplication(cli)
+	command := cli.Args[1]
+	c := &cli
+	if command == "run" {
+		app.Run(c)
+	} else if command == "list" {
+		app.List(c)
+	} else if command == "render" {
+		app.Render(c)
+	} else if command == "render" {
+		app.Help(c)
+	} else {
+		Usage()
+	}
+}
+
+func mainx() {
+	cli := goutils.CLI{os.Args}
+	if len(cli.Args) == 1 {
+		Help(&cli)
+		os.Exit(1)
+	}
 
 	command := cli.Args[1]
 	if len(os.Args) < 2 {
 		Usage()
 	} else {
+		// if command == "test" {
+		// Test(&cli)
 		if command == "help" {
 			Help(&cli)
 		} else if command == "render" {
