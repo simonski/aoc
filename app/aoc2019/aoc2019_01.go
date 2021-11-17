128b2fba423957aa6ce77b93da381dea087101fc
Simon Gauld <simongauld@covey.home>
2021-11-17 09:01:28 +0000

    2019 day 1 part 2 recursion

--- app/aoc2019/aoc2019_01.go
+++ app/aoc2019/aoc2019_01.go
@@ -27,6 +27,18 @@ func (app *Application) FuelRequired(mass int) int {
 	return divided
 }
 
+func (app *Application) FuelRequiredPart2(mass int) int {
+	// recursive
+	result := (mass / 3) - 2
+	if result <= 0 {
+		// fmt.Printf("discounted %v\n", result)
+		return mass
+	} else {
+		// fmt.Printf("remaining  %v\n", result)
+		return mass + app.FuelRequiredPart2(result)
+	}
+}
+
 // rename this to the year and day in question
 func (app *Application) Y2019D01P1() {
 	fmt.Printf("Y2019 D01 ok!\n")
@@ -37,13 +49,35 @@ func (app *Application) Y2019D01P1() {
 		fuelRequired := app.FuelRequired(mass)
 		total += fuelRequired
 	}
-	fmt.Printf("total: %v\n", total)
+	fmt.Printf("Part 1 total: %v\n", total)
 
 }
 
 // rename this to the year and day in question
 func (app *Application) Y2019D01P2() {
-	// 	fmt.Printf("Y2019 D02 ok!\n")
+	fmt.Printf("Y2019 D02 ok!\n")
+	lines := strings.Split(DAY_2019_01_DATA, "\n")
+	total := 0
+	for _, line := range lines {
+		mass, _ := strconv.Atoi(line)
+		fuelRequired := app.FuelRequiredPart2(mass) - mass
+		total += fuelRequired
+	}
+	fmt.Printf("Part 2 total: %v\n", total)
+
+	// answer := app.FuelRequiredPart2(100756) - 100756
+	// fmt.Printf("Y2019 D02 Test: 100756 (requires 50346), our answer: %v\n", answer)
+
+	// fmt.Printf("Y2019 D02 ok!\n")
+	// lines := strings.Split(DAY_2019_01_DATA, "\n")
+	// total := 0
+	// for _, line := range lines {
+	// 	mass, _ := strconv.Atoi(line)
+	// 	fuelRequired := app.FuelRequiredPart2(mass)
+	// 	total += fuelRequired
+	// }
+	// fmt.Printf("Part 2 total: %v\n", total)
+
 }
 
 // rename and uncomment this to the year and day in question once complete for a gold star!
