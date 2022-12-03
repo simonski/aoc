b2356d6e0e5a26910c471af6356fb988b9773b78
Simon Gauld <simongauld@covey.home>
2022-12-03 17:09:31 +0000

    improved test output 2022_03

--- app/aoc2022/aoc2022_03_test.go
+++ app/aoc2022/aoc2022_03_test.go
@@ -1,8 +1,6 @@
 package aoc2022
 
 import (
-	"fmt"
-	"log"
 	"strings"
 	"testing"
 )
@@ -11,8 +9,7 @@ func Test_AOC2022_03_Part1(t *testing.T) {
 	data := DAY_2022_03_TEST_DATA
 	splits := strings.Split(data, "\n")
 	rs := NewRucksack(splits[0])
-	fmt.Printf("SumCommon: %v\n", rs.SumCommon())
-	log.Fatal("fooo")
+	t.Logf("SumCommon: %v\n", rs.SumCommon())
 }
 
 func Test_AOC2022_03_Part11(t *testing.T) {
@@ -23,12 +20,8 @@ func Test_AOC2022_03_Part11(t *testing.T) {
 		rs := NewRucksack(split)
 		rs.DebugCommon()
 		total += rs.SumCommon()
-		// fmt.Printf("SumCommon: %v\n", rs.SumCommon())
-		// log.Fatal("fooo")
-		// rs.Debug()
 	}
-	fmt.Printf("Sum is %v\n", total)
-	log.Fatal("fooo")
+	t.Logf("Sum is %v\n", total)
 }
 
 func Test_AOC2022_03(t *testing.T) {
@@ -40,8 +33,7 @@ func Test_AOC2022_03(t *testing.T) {
 		rs.DebugCommon()
 		total += rs.SumCommon()
 	}
-	fmt.Printf("Sum is %v\n", total)
-	log.Fatal("fooo")
+	t.Logf("Sum is %v\n", total)
 }
 
 func Test_AOC2022_03_Part2_Test(t *testing.T) {
@@ -57,20 +49,19 @@ func Test_AOC2022_03_Part2_Test(t *testing.T) {
 		f2 := rs2.Frequency()
 		f3 := rs3.Frequency()
 
-		fmt.Printf("f1: %v\n", f1)
-		fmt.Printf("f2: %v\n", f2)
-		fmt.Printf("f3: %v\n", f3)
+		t.Logf("f1: %v\n", f1)
+		t.Logf("f2: %v\n", f2)
+		t.Logf("f3: %v\n", f3)
 
 		for i := 0; i < len(f1); i++ {
 			if f1[i] > 0 && f2[i] > 0 && f3[i] > 0 {
 				value := (i + 1)
 				total += value
-				fmt.Printf("group[%v] shares %v, int value %v, total=%v\n", index/3, AZ[i:i+1], value, total)
+				t.Logf("group[%v] shares %v, int value %v, total=%v\n", index/3, AZ[i:i+1], value, total)
 			}
 		}
 	}
-	fmt.Printf("total is %v\n", total)
-	log.Fatal("fooo")
+	t.Logf("total is %v\n", total)
 }
 
 func Test_AOC2022_03_Part2_TestX(t *testing.T) {
@@ -86,18 +77,17 @@ func Test_AOC2022_03_Part2_TestX(t *testing.T) {
 		f2 := rs2.Frequency()
 		f3 := rs3.Frequency()
 
-		fmt.Printf("f1: %v\n", f1)
-		fmt.Printf("f2: %v\n", f2)
-		fmt.Printf("f3: %v\n", f3)
+		t.Logf("f1: %v\n", f1)
+		t.Logf("f2: %v\n", f2)
+		t.Logf("f3: %v\n", f3)
 
 		for i := 0; i < len(f1); i++ {
 			if f1[i] > 0 && f2[i] > 0 && f3[i] > 0 {
 				value := (i + 1)
 				total += value
-				fmt.Printf("group[%v] shares %v, int value %v, total=%v\n", index/3, AZ[i:i+1], value, total)
+				t.Logf("group[%v] shares %v, int value %v, total=%v\n", index/3, AZ[i:i+1], value, total)
 			}
 		}
 	}
-	fmt.Printf("total is %v\n", total)
-	log.Fatal("fooo")
+	t.Logf("total is %v\n", total)
 }
