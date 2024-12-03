cc377222439b4985bf7f2edc71feaab613395b53
simon gauld <simon.gauld@gmail.com>
2024-12-03 06:30:55 +0000

    2024_03

---
+++ app/aoc2024/d3/program.go
@@ -0,0 +1,59 @@
+package d3
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/simonski/aoc/utils"
+)
+
+/*
+Day 3: Mull It Over
+*/
+
+type Puzzle struct {
+	title string
+	year  int
+	day   int
+	input string
+	lines []string
+}
+
+func (puzzle *Puzzle) GetSummary() utils.Summary {
+	s := utils.Summary{Day: puzzle.day, Year: puzzle.year, Name: puzzle.title}
+	s.ProgressP1 = utils.NotStarted
+	s.ProgressP2 = utils.NotStarted
+	s.DateStarted = "2024-12-03 06:29:08"
+	return s
+}
+
+func NewPuzzleWithData(input string) *Puzzle {
+	iyear, _ := strconv.Atoi("2024")
+	iday, _ := strconv.Atoi("3")
+	p := Puzzle{year: iyear, day: iday, title: "Day 3: Mull It Over"}
+	p.Load(input)
+	return &p
+}
+
+func NewPuzzle() *Puzzle {
+	return NewPuzzleWithData(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Load(input string) {
+	lines := strings.Split(input, "\n")
+	puzzle.input = input
+	puzzle.lines = lines
+}
+
+func (puzzle *Puzzle) Part1() {
+	puzzle.Load(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Part2() {
+	puzzle.Load(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Run() {
+	puzzle.Part1()
+	puzzle.Part2()
+}
