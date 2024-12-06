3bd7cee14e14dd3d008e2522c742e68bca409a0f
simon gauld <simon.gauld@gmail.com>
2024-12-06 08:19:15 +0000

    2024 06 start

---
+++ app/aoc2024/d6/blog.md
@@ -0,0 +1,4 @@
+Puzzle: Day 6: Guard Gallivant
+Year: 2024
+Day: 6
+
---
+++ app/aoc2024/d6/program.go
@@ -0,0 +1,59 @@
+package d6
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/simonski/aoc/utils"
+)
+
+/*
+Day 6: Guard Gallivant
+*/
+
+type Puzzle struct {
+	title string
+	year  int
+	day   int
+	input string
+	lines []string
+}
+
+func (puzzle *Puzzle) GetSummary() utils.Summary {
+	s := utils.Summary{Day: puzzle.day, Year: puzzle.year, Name: puzzle.title}
+	s.ProgressP1 = utils.NotStarted
+	s.ProgressP2 = utils.NotStarted
+	s.DateStarted = "2024-12-06 08:15:03"
+	return s
+}
+
+func NewPuzzleWithData(input string) *Puzzle {
+	iyear, _ := strconv.Atoi("2024")
+	iday, _ := strconv.Atoi("6")
+	p := Puzzle{year: iyear, day: iday, title: "Day 6: Guard Gallivant"}
+	p.Load(input)
+	return &p
+}
+
+func NewPuzzle() *Puzzle {
+	return NewPuzzleWithData(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Load(input string) {
+	lines := strings.Split(input, "\n")
+	puzzle.input = input
+	puzzle.lines = lines
+}
+
+func (puzzle *Puzzle) Part1() {
+	puzzle.Load(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Part2() {
+	puzzle.Load(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Run() {
+	puzzle.Part1()
+	puzzle.Part2()
+}
