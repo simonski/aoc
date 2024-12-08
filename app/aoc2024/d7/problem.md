6c751ec5ecfd2d33c3b27724629720c8dcdb07ae
simon gauld <simon.gauld@lseg.com>
2024-12-08 09:20:45 +0000

    2024 07 finished

--- app/aoc2024/d7/problem.md
+++ app/aoc2024/d7/problem.md
@@ -31,3 +31,20 @@ The engineers just need the total calibration result, which is the sum of the te
 
 Determine which equations could possibly be true. What is their total calibration result?
 
+
+
+--- Part Two ---
+The engineers seem concerned; the total calibration result you gave them is nowhere close to being within safety tolerances. Just then, you spot your mistake: some well-hidden elephants are holding a third type of operator.
+
+The concatenation operator (||) combines the digits from its left and right inputs into a single number. For example, 12 || 345 would become 12345. All operators are still evaluated left-to-right.
+
+Now, apart from the three equations that could be made true using only addition and multiplication, the above example has three more equations that can be made true by inserting operators:
+
+156: 15 6 can be made true through a single concatenation: 15 || 6 = 156.
+7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
+192: 17 8 14 can be made true using 17 || 8 + 14.
+Adding up all six test values (the three that could be made before using only + and * plus the new three that can now be made by also using ||) produces the new total calibration result of 11387.
+
+Using your new knowledge of elephant hiding spots, determine which equations could possibly be true. What is their total calibration result?
+
+
--- app/aoc2024/d7/program.go
+++ app/aoc2024/d7/program.go
@@ -92,10 +92,59 @@ func descend(total int, target int, depth int, values []int) bool {
 }
 
 func (puzzle *Puzzle) Part2() {
-	puzzle.Load(REAL_DATA)
+	puzzle.p2_test(TEST_DATA)
+	puzzle.p2_test(REAL_DATA)
+}
+
+func (puzzle *Puzzle) p2_test(data string) {
+	puzzle.Load(data)
+	total := 0
+	for _, line := range puzzle.lines {
+		splits := strings.Split(line, ":")
+		target, _ := strconv.Atoi(splits[0])
+		v := strings.Trim(splits[1], " ")
+		values := utils.SplitDataToListOfInts(v, " ")
+		if isValidP2(target, values) {
+			total += target
+		}
+		// if isValid(target, values) {
+		// 	total += target
+		// }
+	}
+	fmt.Println(total)
+}
+
+func isValidP2(target int, values []int) bool {
+	return descendP2(values[0], target, 1, values)
+}
+
+func descendP2(total int, target int, depth int, values []int) bool {
+	value := values[depth]
+	cc_total_str := fmt.Sprintf("%v%v", total, value)
+	cc_total, _ := strconv.Atoi(cc_total_str)
+	add_total := total + value
+	mul_total := total * value
+
+	// prefix := strings.Repeat(" ", depth)
+	// fmt.Printf(prefix+"total=%v, target=%v, value=%v, depth=%v/%v, values=%v, add_total=%v, mul_total=%v, cc_total=%v\n", total, target, value, depth, len(values), values, add_total, mul_total, cc_total)
+	if depth+1 == len(values) {
+		if cc_total == target || add_total == target || mul_total == target {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		new_depth := depth + 1
+		a := descendP2(add_total, target, new_depth, values)
+		b := descendP2(mul_total, target, new_depth, values)
+		c := descendP2(cc_total, target, new_depth, values)
+		return a || b || c
+	}
+
 }
 
 func (puzzle *Puzzle) Run() {
-	puzzle.Part1()
+	// puzzle.Part1()
+	// fmt.Println()
 	puzzle.Part2()
 }
