03827755a3e250c574cb1297dece50fbf4e227ce
simon gauld <simon.gauld@lseg.com>
2024-12-08 09:23:06 +0000

    2024 8 start

---
+++ app/aoc2024/d8/blog.md
@@ -0,0 +1,4 @@
+Puzzle: Day 8: Resonant Collinearity
+Year: 2024
+Day: 8
+
---
+++ app/aoc2024/d8/program.go
@@ -0,0 +1,59 @@
+package d8
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/simonski/aoc/utils"
+)
+
+/*
+Day 8: Resonant Collinearity
+*/
+
+type Puzzle struct {
+	title string
+	year  int
+	day   int
+	input string
+	lines []string
+}
+
+func (puzzle *Puzzle) GetSummary() utils.Summary {
+	s := utils.Summary{Day: puzzle.day, Year: puzzle.year, Name: puzzle.title}
+	s.ProgressP1 = utils.NotStarted
+	s.ProgressP2 = utils.NotStarted
+	s.DateStarted = "2024-12-08 09:20:58"
+	return s
+}
+
+func NewPuzzleWithData(input string) *Puzzle {
+	iyear, _ := strconv.Atoi("2024")
+	iday, _ := strconv.Atoi("8")
+	p := Puzzle{year: iyear, day: iday, title: "Day 8: Resonant Collinearity"}
+	p.Load(input)
+	return &p
+}
+
+func NewPuzzle() *Puzzle {
+	return NewPuzzleWithData(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Load(input string) {
+	lines := strings.Split(input, "\n")
+	puzzle.input = input
+	puzzle.lines = lines
+}
+
+func (puzzle *Puzzle) Part1() {
+	puzzle.Load(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Part2() {
+	puzzle.Load(REAL_DATA)
+}
+
+func (puzzle *Puzzle) Run() {
+	puzzle.Part1()
+	puzzle.Part2()
+}
