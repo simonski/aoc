60055dfb51687dc5471b04c7509d288d005fe9ee
simon gauld <simon.gauld@lseg.com>
2024-12-09 18:05:00 +0000

    2024 09 done

--- app/aoc2024/d9/program.go
+++ app/aoc2024/d9/program.go
@@ -50,7 +50,7 @@ type Disk struct {
 	last_swap int
 }
 
-func NewDisk(data string) *Disk {
+func NewDiskP1(data string) *Disk {
 	id := 0
 	blocks := make([]*Block, 0)
 	for index := 0; index < len(data); index++ {
@@ -73,6 +73,27 @@ func NewDisk(data string) *Disk {
 	return &disk
 }
 
+func NewDiskP2(data string) *Disk {
+	id := 0
+	blocks := make([]*Block, 0)
+	for index := 0; index < len(data); index++ {
+		value_str := data[index : index+1]
+		value, _ := strconv.Atoi(value_str)
+		if index%2 == 0 {
+			block := NewBlock(id)
+			block.size = value
+			blocks = append(blocks, block)
+			id += 1
+		} else {
+			block := NewBlock(-1)
+			block.size = value
+			blocks = append(blocks, block)
+		}
+	}
+	disk := Disk{blocks: blocks, last_swap: -999}
+	return &disk
+}
+
 func (disk *Disk) Defrag(debug bool) {
 	if debug {
 		disk.debug()
@@ -88,6 +109,25 @@ func (disk *Disk) Defrag(debug bool) {
 	}
 }
 
+func (disk *Disk) DefragP2(debug bool) {
+
+	if debug {
+		disk.debugP2()
+	}
+
+	max_file_id := 0
+	for _, block := range disk.blocks {
+		max_file_id = utils.MaxInt(max_file_id, block.id)
+	}
+	for file_id := max_file_id; file_id >= 0; file_id-- {
+		disk.stepP2(file_id)
+		if debug {
+			disk.debugP2()
+		}
+	}
+
+}
+
 func (disk *Disk) checksum() int {
 	result := 0
 	for index := 0; index < len(disk.blocks); index++ {
@@ -95,6 +135,27 @@ func (disk *Disk) checksum() int {
 		if block.id > -1 {
 			value := index * block.id
 			result += value
+		}
+	}
+	return result
+}
+
+func (disk *Disk) checksumP2() int {
+	result := 0
+	index := 0
+	for block_index := 0; block_index < len(disk.blocks); block_index++ {
+		block := disk.blocks[block_index]
+		if block.id > -1 {
+			for i := 0; i < block.size; i++ {
+				value := index * block.id
+				result += value
+				index += 1
+			}
+		} else {
+			for i := 0; i < block.size; i++ {
+				index += 1
+			}
+
 		}
 	}
 	return result
@@ -120,14 +181,26 @@ func (disk *Disk) get_first_space() (int, *Block) {
 	return -1, nil
 }
 
+func (disk *Disk) get_first_available_space(min_size int) (int, *Block) {
+	for index := 0; index < len(disk.blocks); index++ {
+		block := disk.blocks[index]
+		if block.id == -1 && block.size >= min_size {
+			return index, block
+		}
+	}
+	return -1, nil
+}
+
 func (disk *Disk) swap(index1 int, index2 int) {
 	b1 := disk.blocks[index1]
 	b2 := disk.blocks[index2]
+
 	id := b1.id
 	size := b1.size
 
 	b1.id = b2.id
 	b1.size = b2.size
+
 	b2.id = id
 	b2.size = size
 }
@@ -146,6 +219,79 @@ func (disk *Disk) step() bool {
 	return false
 }
 
+func (disk *Disk) get_block(file_id int) (int, *Block) {
+	for block_index, block := range disk.blocks {
+		if block.id == file_id {
+			return block_index, block
+		}
+	}
+	return -1, nil
+}
+
+func (disk *Disk) stepP2(file_id int) bool {
+	// fmt.Printf("stepP2(file_id=%v)\n", file_id)
+	block_index, block := disk.get_block(file_id)
+	space_index, space := disk.get_first_available_space(block.size)
+	if space_index == -1 {
+		return false
+	} else if space_index > block_index {
+		// fmt.Printf("step(file_id=%v, block_index=%v, block_size=%v, no space available)\n", file_id, block_index, block.size)
+		return false
+	}
+	// fmt.Printf("step(file_id=%v, block_index=%v, block_size=%v, space_index=%v, space_size=%v)\n", file_id, block_index, block.size, space_index, space.size)
+	if space.size == block.size {
+		// matches exactly
+		// fmt.Printf("stepP2(%v) perfect match()\n", file_id)
+		disk.swap(block_index, space_index)
+		return true
+	} else if space.size < block.size {
+		// too big, move on
+		// fmt.Printf("stepP2(%v) no spaces()\n", file_id)
+		return false
+	} else {
+		// the space is "too big" so we need to
+		//   split the space into two
+		//   swap the first bit with the block
+		// fmt.Printf("stepP2(%v) space too big (size=%v), split it()\n", file_id, space.size)
+		// fmt.Printf("pre-split, block count is %v\n", len(disk.blocks))
+		disk.split_space(space_index, block.size)
+		// fmt.Printf("post-split, block count is %v\n", len(disk.blocks))
+		block_index, block := disk.get_block(file_id)
+		space_index, _ := disk.get_first_available_space(block.size)
+		// fmt.Printf("stepP2(%v) swapping space_index=%v for block_index=%v\n", file_id, space_index, block_index+1)
+		// fmt.Println("pre-swap:")
+		// disk.debugP2()
+		disk.swap(space_index, block_index)
+		// fmt.Println("post-swap:")
+		// disk.debugP2()
+		fmt.Println()
+
+	}
+	return false
+}
+
+// takes a contiguous space blocksand splits into two blocks, the first of size 'size'
+func (disk *Disk) split_space(index int, size int) {
+	block := disk.blocks[index]
+	lblock := NewBlock(-1)
+	lblock.size = size
+	rblock := NewBlock(-1)
+	rblock.size = block.size - size
+	// fmt.Printf("split_space(index=%v, size=%v), lblock.size=%v\n", index, size, lblock.size)
+	// fmt.Printf("split_space(index=%v, size=%v), rblock.size=%v\n", index, size, rblock.size)
+
+	blocks := make([]*Block, 0)
+	for i := 0; i < len(disk.blocks); i++ {
+		if i != index {
+			blocks = append(blocks, disk.blocks[i])
+		} else {
+			blocks = append(blocks, lblock)
+			blocks = append(blocks, rblock)
+		}
+	}
+	disk.blocks = blocks
+}
+
 func (disk *Disk) debug() {
 	for _, b := range disk.blocks {
 		id_str := fmt.Sprintf("%v", b.id)
@@ -158,11 +304,31 @@ func (disk *Disk) debug() {
 	fmt.Println()
 }
 
+func (disk *Disk) debugP2() {
+	for _, b := range disk.blocks {
+		id_str := fmt.Sprintf("%v", b.id)
+		if b.id > -1 {
+			output := strings.Repeat(id_str, b.size)
+			fmt.Print(output)
+		} else {
+			output := strings.Repeat(".", b.size)
+			fmt.Print(output)
+		}
+	}
+	fmt.Println()
+}
 func (puzzle *Puzzle) p1(data string, debug bool) int {
 	fmt.Println()
-	d := NewDisk(data)
+	d := NewDiskP1(data)
 	d.Defrag(debug)
 	return d.checksum()
+}
+
+func (puzzle *Puzzle) p2(data string, debug bool) int {
+	fmt.Println()
+	d := NewDiskP2(data)
+	d.DefragP2(debug)
+	return d.checksumP2()
 }
 
 func NewPuzzle() *Puzzle {
@@ -185,11 +351,13 @@ func (puzzle *Puzzle) Part1() {
 }
 
 func (puzzle *Puzzle) Part2() {
-	// puzzle.p2(TEST_DATA)
-	// puzzle.p2(REAL_DATA)
+	answer1 := puzzle.p2(TEST_DATA_1, true)
+	answer2 := puzzle.p2(TEST_DATA_2, true)
+	answer3 := puzzle.p2(REAL_DATA, false)
+	fmt.Printf("a1: %v\na2: %v\na3: %v\n", answer1, answer2, answer3)
 }
 
 func (puzzle *Puzzle) Run() {
-	puzzle.Part1()
+	// puzzle.Part1()
 	puzzle.Part2()
 }
